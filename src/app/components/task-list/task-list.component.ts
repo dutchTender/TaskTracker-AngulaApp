802bc43b7831105d528d7eebf426239c7765dea9
li zhang <lzhang421@gmail.com>
2024-12-22 19:45:25 -0500

    completed task update front end synchronization with back end update.

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -20,26 +20,21 @@ export class TaskListComponent implements OnInit {
     /* call get tasks service, then update component rendering via updating component property this.tasks*/
     this.tService.getTasksFromService().subscribe((returnedTasks) => this.tasks = returnedTasks );
   }
-
   taskDeleteEventCatcher(task: Task): void{
     /* call delete service, then update component rendering via updating component property this.tasks */
     this.tService.removeTaskFromService(task).subscribe((deletedTask) => (this.tasks = this.tasks.filter( (t: Task) => t.id !== task.id)) );
-  }
-
-  taskUpdateReminderEventCatcher(task: Task): void{
-    task.reminder = !task.reminder;
-    this.tService.updateTaskToService(task).subscribe( (t) => alert('database updated for task : ' + t.text));
   }
   taskUpdateFormOpenEventCatcher(task: Task): void{
     this.focusedTask = task;
     this.UIService.openEditForm();
   }
-
   taskAddEventCatcher(task: Task): void{
     this.tService.addTaskToService(task).subscribe((addedTask: Task) => (this.tasks.push(addedTask)));
   }
   taskUpdateEventCatcher(task: Task): void{
-    this.tService.updateTaskToService(task).subscribe((addedTask: Task) => (alert('updated db for task ' + addedTask.text + ' still need to update front end')));
+    this.tService.updateTaskToService(task).subscribe((addedTask: Task) => (
+      this.tService.getTasksFromService().subscribe((returnedTasks) => this.tasks = returnedTasks )
+    ));
   }
 
 }
