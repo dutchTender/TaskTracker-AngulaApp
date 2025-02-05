f892a6bd5a04502a9074524d1c200351d724450f
li zhang <lzhang421@gmail.com>
2025-02-04 21:15:55 -0500

    created application actions to support ngRx.

---
+++ src/app/components/ngRx/actions/taskActions.ts
@@ -0,0 +1,24 @@
+import {createAction, props} from '@ngrx/store';
+import {Task} from '../../interfaces/Task';
+
+export const openNewTaskForm = createAction('[Tasks] Open New Task Form');
+export const openEditTaskForm = createAction('[Tasks] Open Edit Task Form');
+
+export const getTasks = createAction('[Task] Get task list');
+export const getTasksSuccess = createAction('[Task] Get task success', props<{userTasks: Task[]}>());
+export const getTasksFailure = createAction('[Task] Get task success', props<{tasksError: string}>());
+
+
+export const createTask = createAction('[Task] Create task');
+export const createTaskSuccess = createAction('[Task] Create task success');
+export const createTaskFailure = createAction('[Task] Create task failure');
+
+export const updateTask = createAction('[Task] Update task');
+export const updateTaskSuccess = createAction('[Task] Update task success');
+export const updateTaskFailure = createAction('[Task] Update task failure');
+
+export const deleteTask = createAction('[Task] Delete task');
+export const deleteTaskSuccess = createAction('[Task] Delete task success');
+export const deleteTaskFailure = createAction('[Task] Delete task failure');
+
+
