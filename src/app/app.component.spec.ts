e49d5f095b2b280b2371bfdb7b48a3896227388d
li zhang <lzhang421@gmail.com>
2025-02-09 17:35:03 -0500

    test for component input injection for task item.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,11 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('task-tracker-app');
   });
-
+/*
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('task-tracker-app app is running!');
-  });
+  });*/
 });
--- src/app/components/task-item/task-item.component.spec.ts
+++ src/app/components/task-item/task-item.component.spec.ts
@@ -1,25 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {FontAwesomeTestingModule} from '@fortawesome/angular-fontawesome/testing';
 import { TaskItemComponent } from './task-item.component';
+import { Component } from '@angular/core';
+import {Task} from '../interfaces/Task';
 
+
+/*below component is used to mock the incoming task value that is injected into this component*/
+@Component({
+  selector: 'app-host-component',
+  template: '<app-task-item [task]="testTask"></app-task-item>t>',
+})
+class TaskHostComponent {
+  testTask: Task =  {
+    day: 'test', reminder: false, text: 'test'
+
+  };
+}
 describe('TaskItemComponent', () => {
   let component: TaskItemComponent;
-  let fixture: ComponentFixture<TaskItemComponent>;
-
+  let fixture: ComponentFixture<TaskHostComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskItemComponent ]
+      declarations: [ TaskItemComponent, TaskHostComponent],
+      imports: [FontAwesomeTestingModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TaskItemComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TaskHostComponent);
+    component = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component ', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
