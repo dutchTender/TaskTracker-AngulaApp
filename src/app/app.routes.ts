21d65cc9731010cb86afae2f057e7266012d5d0d
li zhang <lzhang421@gmail.com>
2025-02-28 15:50:46 -0500

    created empty user list component and added it to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import {Routes} from '@angular/router';
 import {TaskListComponent} from './feature/task/task-list/task-list.component';
 import {AboutPageComponent} from './components/about-page/about-page.component';
+import {UserListComponent} from './feature/user/user-list/user-list.component';
 
 export  const appRoutes: Routes = [
   {
     path: 'tasks', component: TaskListComponent
+  },
+  {
+    path: 'users', component: UserListComponent
   },
   {
     path: '', redirectTo: 'tasks', pathMatch: 'full'
