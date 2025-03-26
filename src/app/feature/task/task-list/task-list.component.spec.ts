52a595ac68450c60078fab4635b1a3ce86f1e572
li zhang <lzhang421@gmail.com>
2025-03-26 10:33:02 -0400

    updated unit test for task-list.component.spec.ts

--- src/app/feature/task/task-list/task-list.component.spec.ts
+++ src/app/feature/task/task-list/task-list.component.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskListComponent } from './task-list.component';
+import { Store } from '@ngrx/store';
+import {of} from 'rxjs';
+import {EditTaskFormComponent} from '../forms/edit-task-form/task-edit-form.component';
+import {AddTaskFormComponent} from '../forms/add-task-form/add-task-form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
-
+  let store: jasmine.SpyObj<Store>;
   beforeEach(async () => {
+
+    store = jasmine.createSpyObj('store', ['select', 'dispatch', 'pipe']);
+    store.select.and.returnValue(of({data: 'fake data'}));
     await TestBed.configureTestingModule({
-      declarations: [ TaskListComponent ]
+      declarations: [ TaskListComponent, EditTaskFormComponent, AddTaskFormComponent ],
+      providers: [{provide: Store, useValue: store}],
+      imports: [FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
   });
@@ -21,5 +31,10 @@ describe('TaskListComponent', () => {
  we will check for the nth element to see if it exists in the html*/
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should select data from the store', () => {
+    component.ngOnInit();
+    expect(store.pipe).toHaveBeenCalled();
   });
 });
